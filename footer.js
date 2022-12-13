1becfb41414caf6a1128b82bb260bab068c0c911
chirag <chiragban24@gmail.com>
2022-12-13 16:33:07 +0530

    first commit

---
+++ footer.js
@@ -0,0 +1,12 @@
+function footer(id){
+    let table
+    let footer = document.getElementById(id);
+let div=document.createElement('div');
+let hr=document.createElement('hr');
+let p=document.createElement('p');
+p.innerHTML='copyright @Ecommerce 2022-23';
+div.appendChild(hr);
+div.appendChild(p);
+footer.appendChild(div);
+}
+
