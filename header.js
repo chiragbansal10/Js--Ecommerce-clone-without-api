fa686513735a42b017bad839807c5c486e09e4c2
chirag <chiragban24@gmail.com>
2022-12-21 22:44:51 +0530

    DYNAMIC HEADER

--- header.js
+++ header.js
@@ -1,7 +1,37 @@
-function header(id){
+header();
+function header(){
+const nav= document.createElement('nav')
+const logo=document.createElement('div').classList='logo' 
+const h1= document.createElement('h1')
+const ecommerce = document.write("Ecommerce")
+
+const search =document.createElement('div').className='search'
+const input= document.createElement('input').id='search'
+input.setAttribute("type", "text")
+input.setAttribute("value","Product name, Category name, etc." )
+const button=document.createElement('button').value='search'
+// const div= document.createElement('div')
+const login_bucket=document.createElement('div').className='login_bucket'
+
+const login= document.createElement('button').innerHTML='login'
+const bucket =document.createElement('button')
+
+
+nav.appendChild(logo)
+logo.appendChild(h1)
+h1.appendChild(ecommerce)
+nav.appendChild(search)
+search.appendChild(input)
+nav.appendChild(button)
+nav.appendChild(login_bucket)
+nav.appendChild(login)
+nav.appendChild(bucket)
+
+const product_navbar=document.createElement('div').className='product_navbar'
+const service=document.createElement('div').className='service'
 
 // const product_service=document.querySelectorAll('.product_service')
-const product_service=document.querySelectorAll(id)
+const product_service=document.querySelectorAll()
 console.log(product_service)
 for(let i of product_service)
 {
